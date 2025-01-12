56096fde34dc573f6d0e2a11f075e6e496c43f38
Cody Lowinske <code7y7@gmail.com>
2025-01-12 12:19:56 -0600

    Initial commit

---
+++ src/main/java/net/code7y7/sorcerymod/SorceryModClient.java
@@ -0,0 +1,10 @@
+package net.code7y7.sorcerymod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SorceryModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/code7y7/sorcerymod/SorceryModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.code7y7.sorcerymod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SorceryModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
