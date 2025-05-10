d506b30419aa5bedc551269b644a6ad4814f5c33
Cody Lowinske <code7y7@gmail.com>
2025-05-09 22:27:08 -0500

    Added new functionality to ignited. Amplifier of 0 will behave normally. Amplifier of 1 will cause the effect to spread to other entities if they are close enough.

--- src/main/java/net/code7y7/sorcerymod/effect/IgnitedEffect.java
+++ src/main/java/net/code7y7/sorcerymod/effect/IgnitedEffect.java
@@ -3,6 +3,7 @@ package net.code7y7.sorcerymod.effect;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffectCategory;
+import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.server.world.ServerWorld;
 
 public class IgnitedEffect extends StatusEffect {
@@ -10,12 +11,23 @@ public class IgnitedEffect extends StatusEffect {
         super(category, color);
     }
 
+
+    //amplifier 0 behaves normally, amplifier 1 will spread this effect to other entities.
     @Override
     public boolean applyUpdateEffect(ServerWorld world, LivingEntity entity, int amplifier) {
         if (!entity.isSubmergedInWater()) {
             entity.setOnFireFor(1.0f);
-        }
 
+            if (amplifier == 1) {
+                world.getOtherEntities(entity, entity.getBoundingBox().expand(0.1), e -> e instanceof LivingEntity)
+                        .forEach(otherEntity -> {
+                            LivingEntity target = (LivingEntity) otherEntity;
+                            if (!target.hasStatusEffect(ModEffects.IGNITED)) {
+                                target.addStatusEffect(new StatusEffectInstance(ModEffects.IGNITED, 100, 1)); // 200 ticks (10s) duration, amplifier 0
+                            }
+                        });
+            }
+        }
         return true;
     }
 
